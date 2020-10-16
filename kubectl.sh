2ece01c5ce1ed0bcd86e4b0ee82046751cd45019
Ondrej Sika <ondrej@ondrejsika.com>
2020-10-16 12:42:10 +0200

    kubectl.sh - download bianary

--- kubectl.sh
+++ kubectl.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-apt-get update && apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-apt-get update
-apt-get install -y kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x ./kubectl && sudo mv ./kubectl /usr/local/bin/kubectl
