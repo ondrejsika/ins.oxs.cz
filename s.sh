2fe0b6aef5b5753e56552499075eef615626b56d
Ondrej Sika <ondrej@ondrejsika.com>
2023-07-21 20:06:38 +0900

    feat: Add curl script for server setup

---
+++ s.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# Server Setup
+
+cd && \
+  apt update && \
+  apt purge -y nano && \
+  apt install -y vim mc htop git sudo curl wget gnupg apt-transport-https && \
+  mkdir -p .ssh && \
+  curl -o - https://github.com/ondrejsika.keys > .ssh/authorized_keys
