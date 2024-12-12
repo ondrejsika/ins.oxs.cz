d85931c57a2d72b3c0d5740cc531cbaccae2b3f0
Ondrej Sika <ondrej@ondrejsika.com>
2024-12-12 21:31:39 +0100

    feat: Append SSH authorized keys in Server Setup

--- s.sh
+++ s.sh
@@ -7,4 +7,4 @@ cd && \
   apt purge -y nano && \
   apt install -y vim mc htop git sudo curl wget gnupg apt-transport-https && \
   mkdir -p .ssh && \
-  curl -o - https://github.com/ondrejsika.keys > .ssh/authorized_keys
+  curl -o - https://github.com/ondrejsika.keys >> ~/.ssh/authorized_keys
