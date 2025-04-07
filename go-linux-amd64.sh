db1af9e6a3b026824c1f75d2aa434a5adc85113c
Ondrej Sika <ondrej@ondrejsika.com>
2025-04-07 17:07:27 +0200

    feat(go-linux-amd64.sh): Add $PATH as variable (not exposed)

--- go-linux-amd64.sh
+++ go-linux-amd64.sh
@@ -4,6 +4,6 @@ VERSION=1.24.2
 wget https://golang.org/dl/go$VERSION.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go$VERSION.linux-amd64.tar.gz
 rm go$VERSION.linux-amd64.tar.gz
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.zshrc
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.zshrc
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
