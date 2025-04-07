e8f760446f9dd8a443c319b6b9b00684aacfedd5
Ondrej Sika <ondrej@ondrejsika.com>
2025-04-07 17:05:40 +0200

    fix(go-linux-amd64.sh): Fix rm file name

--- go-linux-amd64.sh
+++ go-linux-amd64.sh
@@ -3,7 +3,7 @@
 VERSION=1.24.2
 wget https://golang.org/dl/go$VERSION.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go$VERSION.linux-amd64.tar.gz
-rm go$VERSION.tar.gz
+rm go$VERSION.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.zshrc
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
