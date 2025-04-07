1f7ab5e7b54be1f78650cc465cea187f08493ad5
Ondrej Sika <ondrej@ondrejsika.com>
2025-04-07 17:02:03 +0200

    feat(go-linux-amd64.sh): Update to Go v1.24.2

--- go-linux-amd64.sh
+++ go-linux-amd64.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-wget https://golang.org/dl/go1.17.1.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.1.linux-amd64.tar.gz
-rm go1.17.1.linux-amd64.tar.gz
+VERSION=1.24.2
+wget https://golang.org/dl/go$VERSION.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go$VERSION.linux-amd64.tar.gz
+rm go$VERSION.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
